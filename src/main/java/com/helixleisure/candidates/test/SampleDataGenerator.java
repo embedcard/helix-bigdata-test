1cb09aac79b6e55f4c348ee6810f71c8bcf89de2
Janos Schwellach <janos.schwellach@helixleisure.com>
2015-08-18 12:58:24 +0800

    initial import

---
+++ src/main/java/com/helixleisure/candidates/test/SampleDataGenerator.java
@@ -0,0 +1,118 @@
+package com.helixleisure.candidates.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Random;
+
+import org.apache.thrift.TSerializer;
+
+import com.helixleisure.schema.Location;
+
+import static com.helixleisure.candidates.test.Data.*;
+
+public class SampleDataGenerator {
+	public static final String ROOT_FOLDER = "/tmp/candidates/";
+
+	public static TSerializer ser;
+	private static final Random R = new Random();
+	private static long offset = Timestamp.valueOf("2015-05-01 00:00:00").getTime();
+	private static long end = Timestamp.valueOf("2015-08-01 00:00:00").getTime();
+	private static long diff = end - offset + 1;
+	
+	
+	public static final String[] LOCATIONS = new String[] {
+		"Fun Zone",
+		"Party World",
+		"Play Land"
+	};
+	
+	public static final String[] LOCATION_CITY = new String[] {
+		"Perth",
+		"Singapore",
+		"Dubai"
+	};
+	
+	public static final String[] LOCATION_COUNTRY = new String[] {
+		"Australia",
+		"Singapore",
+		"United Arab Emirates"
+	};
+	
+	public static final String[] GAMES = new String[] {
+		"Snapshot",
+		"Speed of Light",
+		"Color Match",
+		"Pinata",
+		"High Five",
+		"The Vault",
+		"Shooting Mania",
+		"Mega Spin",
+		"Stacker",
+		"Balloon Buster"
+	};
+
+	private static File ROOT_FILE;
+	
+	public static TSerializer getSerializer() {
+		if (ser == null) ser = new TSerializer();
+		return ser;
+	}
+	
+	public static Timestamp generateRandomTimeStamp() {
+		Timestamp rand = new Timestamp(offset + (long)(Math.random() * diff));
+		return rand;
+	}
+	
+	public static String getTimestampAsFileSystemString(Timestamp timestamp) {
+		String result = timestamp.toString().replaceAll("\\:", "").replaceAll(" ", "").replaceAll("\\.", "");
+		return result;
+	}
+	
+	
+	public static void initTestData() throws Exception {
+		ROOT_FILE = new File(ROOT_FOLDER);
+		ROOT_FILE.mkdirs();
+		TSerializer ser = getSerializer();
+		for (int location=0;location<LOCATIONS.length;location++) {
+			Timestamp timestamp = Timestamp.valueOf("2015-05-01 00:00:00");
+			write(ser.serialize(makeLocationProperty(timestamp, location, LOCATIONS[location])),"property_location_"+location+"_name_time_"+getTimestampAsFileSystemString(timestamp));
+			Location locationInfo = new Location();
+			locationInfo.setCity(LOCATION_CITY[location]);
+			locationInfo.setCountry(LOCATION_COUNTRY[location]);
+			write(ser.serialize(makeLocationProperty(timestamp, location, locationInfo)),"property_location_"+location+"_location_time_"+getTimestampAsFileSystemString(timestamp));
+			
+			for (int i=0;i<100000;i++) {
+				timestamp = generateRandomTimeStamp();
+				String game = GAMES[R.nextInt(GAMES.length)];
+				write(ser.serialize(makeGameplay(location, game, timestamp)),"edge_gameplay_location_"+location+"_game_"+game+"_time_"+getTimestampAsFileSystemString(timestamp));
+			}
+		}
+	}
+	
+	public static void write(byte[] bytes, String name) {
+		File f = new File(ROOT_FILE,name);
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(f);
+			fos.write(bytes);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		initTestData();
+	}
+}
---
+++ src/main/thrift/schema.thrift
@@ -0,0 +1,56 @@
+namespace java com.helixleisure.schema
+
+union LocationID {
+	1: i64 location_id;
+}
+
+union GameID {
+	1: string name;
+}
+
+struct Location {
+	1: optional string city;
+	2: optional string state;
+	3: optional string country; 
+}
+
+union LocationPropertyValue {
+	1: string location_name;
+	2: Location location;
+}
+
+struct LocationProperty {
+	1: required LocationID id;
+	2: required LocationPropertyValue property;
+}
+
+union GamePropertyValue {
+	1: i32 game_plays;
+}
+
+struct GameProperty {
+	1: required GameID id;
+	2: required GamePropertyValue property;
+}
+
+struct GamePlayEdge {
+	1: required LocationID location;
+	2: required GameID game;
+	3: required i64 nonce;
+}
+
+struct Pedigree {
+	// timestamp of the event in yyyy-MM-dd-HH.mm.ss.nnnnnn
+	1: required string timestamp;
+}
+
+union DataUnit {
+	1: LocationProperty location_property;
+	2: GameProperty game_property;
+	3: GamePlayEdge game_play;
+}
+
+struct Data {
+	1: required Pedigree pedigree;
+	2: required DataUnit dataunit;
+}
